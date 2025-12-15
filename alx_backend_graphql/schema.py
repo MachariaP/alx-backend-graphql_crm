2a31a62c7e885a46f14ecc78b47a9f2bace91793
Phinehas <walburphinehas78@gmail.com>
2025-12-15 13:45:34 +0300

    Add CRMQuery base class for modular schema as required

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,12 @@
 import graphene
 
-class Query(graphene.ObjectType):
+
+class CRMQuery(graphene.ObjectType):
+    pass
+
+
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
+
 
 schema = graphene.Schema(query=Query)
